c1fc5e67f5139b58a6a2641359b21957f6a913d0
adon1as <adonias.dev@gmail.com>
2025-01-23 22:58:41 -0300

    inclusion of borders for camera and player, also death by falling

--- player/player.gd
+++ player/player.gd
@@ -3,9 +3,15 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+var camera
+func _ready() -> void:
+	camera = get_child(2)
 
 func _physics_process(delta: float) -> void:
+	position.x = clamp(position.x, camera.limit_left, camera.limit_right)
+	position.y = clamp(position.y, camera.limit_top, camera.limit_bottom)
+	if position.y == camera.limit_bottom:
+		die()
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -23,3 +29,6 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+func die():
+	get_tree().change_scene_to_file("res://global/main_menu.tscn")
