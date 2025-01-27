fa64d2a31a554e1c056d355f1e32ee9aaf32d674
ChickenOnABoat <ChickenOnABoat@users.noreply.github.com>
2025-01-26 22:35:22 -0300

    parcial cenas base

--- player/player.gd
+++ player/player.gd
@@ -1,12 +1,16 @@
 extends CharacterBody2D
 class_name Player
 
-const SPEED = 300.0
+const SPEED = 100.0
 const JUMP_VELOCITY = -400.0
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+enum PlayerStates { IDLE, RUN }
 
 signal die
 
-var sprite : Sprite2D
 var timers: PLayerTimer
 var gun
 var last_valid_direction = 1
@@ -17,12 +21,13 @@ var in_dash = false
 var in_knockback = false
 var in_delay_dmg = false
 
+var _state: PlayerStates = PlayerStates.IDLE;
+
 #TODO revert logic
 var knockback_origin = Vector2.ZERO
 
 func _ready() -> void:
 	gun = get_node("Sprite2D/BubbleGun")
-	sprite = get_child(1)
 	timers = get_node("PlayerTimer")
 
 func _physics_process(delta: float) -> void:
@@ -42,18 +47,42 @@ func process_moviment(delta: float):
 		velocity.y = JUMP_VELOCITY
 
 	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction > 0:
+		animated_sprite_2d.flip_h = false;
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true;
+	
 	if direction:
 		velocity.x = direction * SPEED
 		if direction != 0:
 			last_valid_direction = direction
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	
+		
 	move_and_slide()	
 	
+	if velocity.x != 0:
+		set_state(PlayerStates.RUN);
+	else:
+		set_state(PlayerStates.IDLE);
+
+
+func set_state(new_state: PlayerStates) -> void:
+	if _state == new_state:
+		return;
+	
+	_state = new_state;
+	match _state:
+		PlayerStates.IDLE:
+			animated_sprite_2d.play("idle");
+		PlayerStates.RUN:
+			animated_sprite_2d.play("run");
+
 func process_action(delta: float):
 	if Input.is_action_just_pressed("ui_base_ranged"):
-		gun.shoot(sprite.scale.x)
+		gun.shoot(sprite_2d.scale.x)
 	if !timers.dash.is_stopped():		
 		global_position.x += SPEED* 1.5 * last_valid_direction * delta
 		velocity.y *= 0
@@ -85,13 +114,13 @@ func stamina_maneger():
 func sprite_maneger():
 	if not is_zero_approx(velocity.x):
 		if velocity.x > 0.0:
-			sprite.scale.x = 0.2
+			sprite_2d.scale.x = 0.2
 		else:
-			sprite.scale.x = -0.2
+			sprite_2d.scale.x = -0.2
 	if !timers.delay_dmg.is_stopped():
-		sprite.visible = !sprite.visible
+		sprite_2d.visible = !sprite_2d.visible
 	else:
-		sprite.visible = true
+		sprite_2d.visible = true
 	
 #TODO use this to base explosion script
 func knockback(delta: float):
